0e6bf77abef1a87eadaecc3728c65cb47b2d4c46
Exilliar <rorywebber1000@gmail.com>
2021-06-16 22:15:35 +0100

    some html form stuff

--- index.js
+++ index.js
@@ -8,3 +8,14 @@ function toggleGrav() {
   console.log("toggle grav");
   gameArea.toggleGrav();
 }
+
+function updateGrav() {
+  console.log("submit");
+  const form = document.getElementById("gravSettings");
+  console.log(form);
+  for(let i = 0; i < form.length; i++) {
+    console.log(`${i}: ${form.elements[i].value}`);
+  }
+  console.log("form.elements:", form.elements);
+  console.log(`gravity: ${form.elements.gravity.value}\nmass: ${form.elements.mass.value}`);
+}
